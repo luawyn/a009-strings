ff749061acdf2a38807fd0cdd218364f4aef3636
Luana Trevizani <luanaftrevizani@gmail.com>
2022-08-31 14:50:33 -0300

    exercicios finalizados

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,20 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = 'Eu não sou supersticioso, mas sou um pouco ________.      '
+
+// # Exercício 2
+
+// Observe a string abaixo.
+
+// ```jsx
+// const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// ```
+
+// A partir dela, execute os passos abaixo:
+
+// a) Remova o excesso de espaços no final da string;
+const novaString = minhaString.trim()
+console.log(novaString)
+// b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
+console.log('Quantidade de caracteres da string: ' + novaString.length)
+console.log('Quantidade de caracteres da string: ' + minhaString.length)
+// c) Substitua os traços `________` por “sticioso”.
+console.log(novaString.replace('________', 'sticioso'))
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,27 @@
 //Crie a const para a frase aqui
+
+// # Exercício 3
+
+// A partir da frase a seguir, faça o que se pede
+
+// ```
+// Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"
+// ```
+
+// a) Crie uma `const` no seu código para guardar a frase (com aspas e tudo);
+const frase =
+  'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+console.log(frase)
+// b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
+const frase2 = frase.replace('verde', 'rosa').replace('azul', 'laranja')
+// frase.replace('azul', 'laranja')
+console.log(frase2)
+// c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+console.log(
+  'A nova string inclui "verde" e "laranja"? ' +
+    frase2.includes('verde', 'laranja')
+)
+// **EXTRA:** tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS”
+fraseNova = 'Jorge tem uma casa verde e com portão azul, com os dizeres: '
+fraseUpper = '"BOAS VINDAS, mas não deixe o gato sair"'
+console.log(fraseNova + fraseUpper.toUpperCase())
